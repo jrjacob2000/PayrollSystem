f9ab98c8f06e0a217965ce215b86a9c648b7c0b9
jrjacob2000 <jrjacob2000@gmail.com>
2014-01-13 21:54:47 +0800

    Update Default.aspx.cs

--- Payroll.Web/Pages/Administration/AccountManagement/Default.aspx.cs
+++ Payroll.Web/Pages/Administration/AccountManagement/Default.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,12 +11,17 @@ namespace Payroll.Web.Pages.Administration.AccountManagement
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //grdAccounts.RowEditing += grdAccounts_RowEditing;
+            grdAccounts.RowDeleting += new GridViewDeleteEventHandler(grdAccounts_RowDeleting);
             grdAccounts.RowCommand += new GridViewCommandEventHandler(grdAccounts_RowCommand);
             lbCreate.Click += lbCreate_Click;
 
             if (!IsPostBack)
                 BindGrid();
+        }
+
+        void grdAccounts_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+           
         }
 
         void grdAccounts_RowCommand(object sender, GridViewCommandEventArgs e)
@@ -25,8 +30,22 @@ namespace Payroll.Web.Pages.Administration.AccountManagement
             {
                 int rowindex = Convert.ToInt32(e.CommandArgument);
                 string id = grdAccounts.DataKeys[rowindex].Value.ToString();
+         
 
                 Response.Redirect("~/Pages/Administration/AccountManagement/update.aspx?id=" + id);
+            }
+            else if (e.CommandName == "Delete")
+            {
+                int rowindex = Convert.ToInt32(e.CommandArgument);
+                Guid id = new Guid( grdAccounts.DataKeys[rowindex].Value.ToString());
+
+                DataAccess.Security.DAccounts daAccount = new DataAccess.Security.DAccounts();
+                daAccount.DeleteAccount(id);
+
+                var acct = daAccount.Get(id);
+
+                SetMessage(MessageType.Succes, string.Format("<b>{0}</b> was deleted successfully", acct.UserName));
+                BindGrid();
             }
         }
 
