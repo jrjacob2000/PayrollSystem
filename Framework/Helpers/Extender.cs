e920c1fc105ece19a0a071ada38ec5a8e8cb2006
jrjacob2000 <jrjacob2000@gmail.com>
2014-01-27 21:57:06 +0800

    Update Extender.cs

--- Framework/Helpers/Extender.cs
+++ Framework/Helpers/Extender.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -54,6 +54,24 @@ namespace System.Web
             DateTime.TryParse(dateString, out resultDate);
             
             return resultDate;           
+        }
+
+        public static DateTime GetFirstDayOfWeek(this DateTime sourceDateTime)
+        {
+            var daysAhead = (DayOfWeek.Sunday - (int)sourceDateTime.DayOfWeek);
+
+            sourceDateTime = sourceDateTime.AddDays((int)daysAhead);
+
+            return sourceDateTime;
+        }
+
+        public static DateTime GetLastDayOfWeek(this DateTime sourceDateTime)
+        {
+            var daysAhead = DayOfWeek.Saturday - (int)sourceDateTime.DayOfWeek;
+
+            sourceDateTime = sourceDateTime.AddDays((int)daysAhead);
+
+            return sourceDateTime;
         }
        
     }
