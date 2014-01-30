f290464bba6813195fa60c5f509344599a075629
jrjacob2000 <jrjacob2000@gmail.com>
2014-01-30 21:44:24 +0800

    Update 14_P_GetTimeSheetByEmployee.sql

--- Payroll.Database/Scripts/1.00.00.0000/14_P_GetTimeSheetByEmployee.sql
+++ Payroll.Database/Scripts/1.00.00.0000/14_P_GetTimeSheetByEmployee.sql
@@ -1,4 +1,7 @@
-
+USE [Payroll]
+GO
+/****** Object:  StoredProcedure [dbo].[P_GetTimeSheetByEmployee]    Script Date: 01/30/2014 17:51:25 ******/
+SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
@@ -7,7 +10,7 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-Create PROCEDURE P_GetTimeSheetByEmployee 
+ALTER PROCEDURE [dbo].[P_GetTimeSheetByEmployee] 
 	@EmployeeId uniqueidentifier,
 	@StartDate date,
 	@EndDate	date
@@ -22,14 +25,25 @@ BEGIN
 	--set  @StartDate = dbo.ufn_GetFirstDayOfWeek('1/22/2014')
 	--set  @EndDate = DATEADD(DAY, 7,@StartDate)
 	
-	
 
-	select t.id, t.EmployeeId, c.Period ReportedDate, t.DateTimeIn, t.DateTimeOut, t.IsDeleted
+	select t.id, 
+		t.EmployeeId, 
+		c.Period ReportedDate, 
+		t.DateTimeIn, 
+		t.DateTimeOut, 
+		t.IsDeleted,
+		case when id is null then 
+			case when DATENAME(weekday, c.Period ) = 'Saturday' or DATENAME(weekday, c.Period ) = 'Sunday' then DATENAME(weekday, c.Period )
+			else 
+				case when c.Period < CONVERT(date, getdate()) then 'Absent' else '' end
+			end
+		else '' end Remarks
 	from F_Calendar(@StartDate,@EndDate) C
 	left join dbo.EmployeeTimeSheet t on c.Period = t.ReportedDate and t.employeeId = @EmployeeId and t.IsDeleted = 0
-	 
+
+
 
 END
-GO
+
 
 
